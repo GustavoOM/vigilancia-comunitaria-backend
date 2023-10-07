ee56d5aff6c70f21943468286b7eeb92a3dd7fcd
Raquel <raqueldepapel@gmail.com>
2023-10-07 15:52:09 -0300

    Removing test

--- src/main/java/scc/vigilancia/comunitaria/Application.java
+++ src/main/java/scc/vigilancia/comunitaria/Application.java
@@ -7,15 +7,8 @@ import scc.vigilancia.comunitaria.services.S3Service;
 @SpringBootApplication
 public class Application {
 
-	private static S3Service service;
-
-	public Application(S3Service service) {
-		Application.service = service;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		service.listS3Buckets();
 	}
 
 }
